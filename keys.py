c91939589b4cb2ad161f8a1c0a511bec67eee57f
Denis <144208071+D3ONISIO@users.noreply.github.com>
2023-10-06 14:50:36 +0200

    Obecnosc 06.10
    Obecnosc + metoda logowania
    ( do poprawy )

--- keys.py
+++ keys.py
@@ -1,1 +1,10 @@
-TOKEN = "MTE0OTIyMjg2ODY3NzMwNDM1Mw.GjX7ar.0eKRKgwpeIWP2PWZgG5gOQMurodn4bkvWIDbh8"
+TOKEN = "MTE0OTIyMjg2ODY3NzMwNDM1Mw.Gj90HL.21lOcbRUF_yOQR5nxtD5p8oAZ1poJMElIcbTWQ"
+
+TeamList = []
+# ЧАТИ
+regChat = 1096411841304993872  # встановити id потрібного чату
+# РЕАКЦІЇ
+em_correct = '✅'
+em_not_correct = '❌'    
+# РОЛІ
+cap_role_id = 1157302057754173572 # встановити id потрібної ролі
--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 import discord
 from discord.ext import commands
 from keys import *
-client = commands.Bot(command_prefix='!', intents=discord.Intents.default())
 
-@client.event
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+@bot.event
 async def on_ready():
-    print("The bot is now ready for use!")
-    print("-------------------")
+    print(f'Logged in as {bot.user.name}')
+    x
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
 
-@client.event
-async def hello(ctx):
-    await ctx.send("Hello")
-    
-client.run(TOKEN)
+    parts = message.content.split('|')
+
+    if len(parts) == 3:
+        TeamName = parts[0]
+        TeamTag = parts[1]
+        TeamManagerId = parts[2].strip()  # Очищуємо вхідний рядок від зайвих пробілів
+
+        try:
+        # Отримуємо сервер, на якому було відправлено повідомлення
+            server = message.guild
+
+        # Отримуємо об'єкт користувача за його ідентифікатором
+            user = await server.fetch_member(int(TeamManagerId))
+
+        # Знаходимо роль, яку потрібно додати за ідентифікатором
+            role = discord.utils.get(server.roles, id=cap_role_id)  # Перед цим потрібно мати змінну TeamManagerRoleId
+
+            if user and role:
+                await user.add_roles(role)
+                await message.channel.send(f'Роль {role.name} була додана користувачу {user.display_name}')
+            else:
+                await message.channel.send('Не вдалося знайти користувача або роль')
+        except Exception as e:
+            await message.channel.send(f'Виникла помилка: {e}')
+    else:
+        await message.channel.send("Заявка подана не правильно")
+
+bot.run(TOKEN)
