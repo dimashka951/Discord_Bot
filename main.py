2e3b6f9abe5b7c948d6084c02db6917ac39902a4
Denis <144208071+D3ONISIO@users.noreply.github.com>
2023-10-10 07:36:18 +0200

    10.10 Obecnosc
    + male prawki w kodzie

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
-    x
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -18,17 +18,14 @@ async def on_message(message):
     if len(parts) == 3:
         TeamName = parts[0]
         TeamTag = parts[1]
-        TeamManagerId = parts[2].strip()  # Очищуємо вхідний рядок від зайвих пробілів
+        TeamManagerId = parts[2].strip()  
 
         try:
-        # Отримуємо сервер, на якому було відправлено повідомлення
             server = message.guild
 
-        # Отримуємо об'єкт користувача за його ідентифікатором
             user = await server.fetch_member(int(TeamManagerId))
 
-        # Знаходимо роль, яку потрібно додати за ідентифікатором
-            role = discord.utils.get(server.roles, id=cap_role_id)  # Перед цим потрібно мати змінну TeamManagerRoleId
+            role = discord.utils.get(server.roles, id=cap_role_id)
 
             if user and role:
                 await user.add_roles(role)
