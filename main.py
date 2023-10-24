a6e1ed30e5c73f6364fd175f5f6c51e4d25612f0
Denys Olshanskyi <denys.olshanskyi@360softwarehouse.pl>
2023-10-24 08:08:33 +0200

    Trying to add comand show_teamlist

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ import os
 
 client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user.name}')
+
 
 async def load_extensions():
     for filename in os.listdir("./modules"):
         if filename.endswith(".py"):
             await client.load_extension(f"modules.{filename[:-3]}")
 
+
 async def setup():
     await load_extensions()
     await client.start(TOKEN)
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(setup())
